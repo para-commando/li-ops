d53dc065c6974b7f42f1556d2c6f790e44969de3
para-commando <anirudh.nayak.172k@gmail.com>
2024-05-26 23:43:18 +0530

    navbar designing 2

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
 
 function Navbar() {
   return (
-    <div>
-      Li-ops: execute missions like a commando 
-    </div>
-  )
+    <nav className='bg-camoflage bg-cover flex justify-between align-middle text-white py-4'>
+      <div className='logo'>
+        <span className='text-2xl font-bold text-xl mx-9 '>
+          li-ops: execute your ops
+        </span>
+      </div>
+      <ul className='flex gap-10 mx-9 text-xl'>
+        <li className='cursor-pointer hover:font-bold transition-all duration-200'>
+          <a href=''>Home</a>
+        </li>
+        <li className='cursor-pointer hover:font-bold transition-all duration-200'>
+          <a href=''>Your Tasks</a>
+        </li>
+      </ul>
+      {/* Li-ops: execute missions like a commando  */}
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
