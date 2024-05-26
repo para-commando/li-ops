5201b3fc56f565ccf15ee6f894da768b3919ced2
para-commando <anirudh.nayak.172k@gmail.com>
2024-05-26 22:49:14 +0530

    navbar design start

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import { useState } from 'react'
- 
+import { useState } from 'react';
+import Navbar from './componets/Navbar';
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div className="container">
-      <div className="bg-red-600">
-        Red background
-      </div>
-    </div>
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/componets/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      Li-ops: execute missions like a commando 
+    </div>
+  )
+}
+
+export default Navbar
