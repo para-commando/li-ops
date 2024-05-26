c77a9c44121d176b22a8a097bf859dd3b533fab9
para-commando <anirudh.nayak.172k@gmail.com>
2024-05-27 00:16:30 +0530

    navbar design 3

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ function App() {
   return (
     <>
       <Navbar />
+      <div className="container mx-auto my-5 rounded-xl bg-gray-800 text-white">
+      its 23:00 hours
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
+    
     extend: {
+      colors: {
+        camo_match: '#242424',
+      },
       backgroundImage: {
         camoflage: "url('src/assets/camo.jpg')",
       },
